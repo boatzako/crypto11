5e6b59c39c7ae5034f6d82f89efa9a666cbdaf68
Teerapat Voradanunt <teerapat.v@kbtg.tech>
2020-10-01 17:06:16 +0700

    Add unit test

--- keys_test.go
+++ keys_test.go
@@ -125,6 +125,102 @@ func TestFindingKeyPairsWithAttributes(t *testing.T) {
 	})
 }
 
+func TestFindDifferentLabelKeyPair(t *testing.T) {
+	withContext(t, func(ctx *Context) {
+		prvLabel := randomBytes()
+		pubLabel := randomBytes()
+
+		key, err := ctx.GenerateRSAKeyPairWithLabel(randomBytes(), prvLabel, rsaSize)
+		require.NoError(t, err)
+		defer func(k Signer) { _ = k.Delete() }(key)
+
+		key, err = ctx.GenerateRSAKeyPairWithLabel(randomBytes(), pubLabel, rsaSize)
+		require.NoError(t, err)
+		defer func(k Signer) { _ = k.Delete() }(key)
+
+		_, err = ctx.FindDifferentLabelKeyPair(nil, nil)
+		assert.Error(t, err)
+
+		_, err = ctx.FindDifferentLabelKeyPair(nil, pubLabel)
+		assert.Error(t, err)
+
+		_, err = ctx.FindDifferentLabelKeyPair(prvLabel, nil)
+		assert.Error(t, err)
+
+		_, err = ctx.FindDifferentLabelKeyPair(prvLabel, pubLabel)
+		require.NoError(t, err)
+	})
+}
+
+func TestFindDifferentLabelKeyPairs(t *testing.T) {
+	withContext(t, func(ctx *Context) {
+		prvLabel := randomBytes()
+		pubLabel := randomBytes()
+
+		key, err := ctx.GenerateRSAKeyPairWithLabel(randomBytes(), prvLabel, rsaSize)
+		require.NoError(t, err)
+		defer func(k Signer) { _ = k.Delete() }(key)
+
+		key, err = ctx.GenerateRSAKeyPairWithLabel(randomBytes(), pubLabel, rsaSize)
+		require.NoError(t, err)
+		defer func(k Signer) { _ = k.Delete() }(key)
+
+		_, err = ctx.FindDifferentLabelKeyPairs(nil, nil)
+		assert.Error(t, err)
+
+		_, err = ctx.FindDifferentLabelKeyPairs(nil, pubLabel)
+		assert.Error(t, err)
+
+		_, err = ctx.FindDifferentLabelKeyPairs(prvLabel, nil)
+		assert.Error(t, err)
+
+		keys, err := ctx.FindDifferentLabelKeyPairs(prvLabel, pubLabel)
+		require.NoError(t, err)
+		require.Len(t, keys, 1)
+	})
+}
+
+func TestFindDifferentLabelKeyPairsWithAttributes(t *testing.T) {
+	withContext(t, func(ctx *Context) {
+
+		label := randomBytes()
+		label1 := randomBytes()
+
+		key, err := ctx.GenerateRSAKeyPairWithLabel(randomBytes(), label, rsaSize)
+		require.NoError(t, err)
+		defer func(k Signer) { _ = k.Delete() }(key)
+		key, err = ctx.GenerateRSAKeyPairWithLabel(randomBytes(), label1, rsaSize)
+		require.NoError(t, err)
+		defer func(k Signer) { _ = k.Delete() }(key)
+		key, err = ctx.GenerateRSAKeyPairWithLabel(randomBytes(), label1, rsaSize)
+		require.NoError(t, err)
+		defer func(k Signer) { _ = k.Delete() }(key)
+
+		pubAttrs := NewAttributeSet()
+		_ = pubAttrs.Set(CkaLabel, label)
+		prvAttrs := NewAttributeSet()
+		_ = prvAttrs.Set(CkaLabel, label)
+
+		keys, err := ctx.FindDifferentLabelKeyPairsWithAttributes(pubAttrs, prvAttrs)
+		require.NoError(t, err)
+		require.Len(t, keys, 1)
+
+		_ = pubAttrs.Set(CkaLabel, label1)
+		_ = prvAttrs.Set(CkaLabel, label1)
+		keys, err = ctx.FindDifferentLabelKeyPairsWithAttributes(pubAttrs, prvAttrs)
+		require.NoError(t, err)
+		require.Len(t, keys, 4)
+
+		pubAttrs = NewAttributeSet()
+		prvAttrs = NewAttributeSet()
+		_ = pubAttrs.Set(CkaKeyType, pkcs11.CKK_RSA)
+		_ = prvAttrs.Set(CkaKeyType, pkcs11.CKK_RSA)
+		keys, err = ctx.FindDifferentLabelKeyPairsWithAttributes(pubAttrs, prvAttrs)
+		require.NoError(t, err)
+		require.Len(t, keys, 9)
+	})
+}
+
 func TestFindingAllKeys(t *testing.T) {
 	withContext(t, func(ctx *Context) {
 		for i := 0; i < 10; i++ {
